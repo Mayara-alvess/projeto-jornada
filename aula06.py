d63b0cb2f5bc8ddac79cc903dad2332b272f5c9c
Mayara-alvess <mayara.elias23@gmail.com>
2025-04-15 21:10:21 -0300

    aula06

---
+++ aula06.py
@@ -0,0 +1,5 @@
+data_usuario = "15/04/2025"
+lista_de_dia_mes_ano = data_usuario.split("/")
+print(f"O elemento 1 é o: {lista_de_dia_mes_ano[0]}")
+print(f"O elemento 2 é o: {lista_de_dia_mes_ano[1]}")
+print(f"O elemento 3 é o: {lista_de_dia_mes_ano[2]}")
